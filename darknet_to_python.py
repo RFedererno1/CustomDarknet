89cc436d08ee17e2db7e871df76ab98b0f406ca3
RFedererno1 <thang.trancntt@hcmut.edu.vn>
2021-03-29 10:24:00 +0700

    add class

--- darknet_to_python.py
+++ darknet_to_python.py
@@ -5,20 +5,26 @@ import cv2
 # CONFIG_PATH = 'darknet_cfg/yolo-obj.cfg'
 # DATA_PATH = 'darknet_cfg/obj1.data'
 # WEIGHT_PATH = '/content/yolo-obj_best_no_cudnn_half.weights'
-CONF_THRESH = 0.25
 
 # network, class_names, _ = darknet.load_network(CONFIG_PATH, DATA_PATH, WEIGHT_PATH, batch_size=1)
+class Detect:
+  def __init__(self, CONFIG_PATH, DATA_PATH, WEIGHT_PATH):
+    self.CONFIG_PATH = CONFIG_PATH
+    self.DATA_PATH = DATA_PATH
+    self.WEIGHT_PATH = WEIGHT_PATH
+    self.CONF_THRESH = 0.25
+    self.network, self.class_names, _ = darknet.load_network(self.CONFIG_PATH, self.DATA_PATH, self.WEIGHT_PATH, batch_size=1)
 
-def detect_obj(image, network, class_names):
-  width = darknet.network_width(network)
-  height = darknet.network_height(network)
-  image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  h, w, _ = image_rgb.shape
-  original_image = darknet.make_image(w, h, 3)
-  darknet.copy_image_from_bytes(original_image, image_rgb.tobytes())
-  resized_image = darknet.letterbox_image(original_image, width, height)
-  detections = darknet.detect_image(network, class_names, resized_image, thresh=CONF_THRESH, w = w, h = h)
-  darknet.free_image(original_image)
-  darknet.free_image(resized_image)
-  detections = [(x[0],x[1],x[2],cv2.cvtColor(image[int(x[2][1] - x[2][3]/2):int(x[2][1] + x[2][3]/2),int(x[2][0] - x[2][2]/2):int(x[2][0] + x[2][2]/2)], cv2.COLOR_BGR2RGB)) for x in detections]
-  return detections
+  def detect_obj(self, image):
+    width = darknet.network_width(self.network)
+    height = darknet.network_height(self.network)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    h, w, _ = image_rgb.shape
+    original_image = darknet.make_image(w, h, 3)
+    darknet.copy_image_from_bytes(original_image, image_rgb.tobytes())
+    resized_image = darknet.letterbox_image(original_image, width, height)
+    detections = darknet.detect_image(self.network, self.class_names, resized_image, thresh=self.CONF_THRESH, w = w, h = h)
+    darknet.free_image(original_image)
+    darknet.free_image(resized_image)
+    detections = [(x[0],x[1],x[2],cv2.cvtColor(image[int(x[2][1] - x[2][3]/2):int(x[2][1] + x[2][3]/2),int(x[2][0] - x[2][2]/2):int(x[2][0] + x[2][2]/2)], cv2.COLOR_BGR2RGB)) for x in detections]
+    return detections
